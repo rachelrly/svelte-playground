b3f2df6b330031f6af9076e9378a96d5e8d17e77
Rachel <rachelreillydev@gmail.com>
2022-02-11 10:10:42 -0600

    completes tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+const isProduction = !process.env.ROLLUP_WATCH
+
 module.exports = {
-  purge: [],
+  purge: {
+    content: ['./src/**/*.svelte'],
+    enabled: isProduction // disable purge in dev
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
   plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true
+  }
 }
